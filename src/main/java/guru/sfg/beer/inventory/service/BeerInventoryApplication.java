64486b2c5508f9ee0527c13ebdd1631d58a8668b
niro1982 <n.edinger1982@gmail.com>
2020-07-18 17:04:11 +0300

    events and listeners

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -3,6 +3,13 @@ package guru.sfg.beer.inventory.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring does component scan from this package(where @SpringBootApplication is).
+ * it would not pick up any spring components outside of this package (in this case all
+ * we have outside of this package is pojos in common.events package so its not a problem.
+ * if we do have components we can tell spring to do a component scan)
+ */
+
 @SpringBootApplication
 public class BeerInventoryApplication {
 
